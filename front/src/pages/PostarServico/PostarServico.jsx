b367179920915f255d7d258b5295e4b5db80ae79
VitorGerrard <87501972+VitorGerrard@users.noreply.github.com>
2024-03-05 18:00:30 -0300

    atualizando páginas
    Co-Authored-By: Leandro Santana <96756735+Leandro-Santana013@users.noreply.github.com>

--- front/src/pages/PostarServico/PostarServico.jsx
+++ front/src/pages/PostarServico/PostarServico.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useNavigate } from 'react-router-dom'
 import { SidebarCliente, Header, TextInput } from "../../components";
 
 import "./postarServico.css"
@@ -6,11 +7,30 @@ import { RiArrowLeftLine, RiListUnordered, RiQuestionLine, RiAttachment2 } from 
 
 
 const PostarServico = () => {
+    const navigate = useNavigate();
     const [text, setText] = useState("");
+    const [form, setform] = useState(1)
     const handleChange = (newValue) => {
         console.log("Novo valor:", newValue);
         setText(newValue);
     };
+    const handleNext = () => {
+        setform(form + 1);
+    };
+    const handleBefore = () => {
+        if (form === 1) {
+            // If form is 1, go back to homeCliente
+            navigate("/homecliente");
+        } else {
+            // If form is greater than 1, decrement the form value
+            setform(form - 1);
+            console.log(form)
+        }
+    };
+
+
+    
+
     return (
         <>
             <Header />
@@ -20,162 +40,167 @@ const PostarServico = () => {
                     <div className="publicarHeader">
                         <h2 className="publicarTitulo">Publique um serviço</h2>
                         <div className="publicarPassos">
-                            <div className="publicar123">1</div>
-                            <div className="publicar123">2</div>
-                            <div className="publicar123">3</div>
+                            <div className={`publicar123 ${form === 1 ? 'form' : ''}`}>1</div>
+                            <div className={`publicar123 ${form === 2 ? 'form' : ''}`}>2</div>
+                            <div className={`publicar123 ${form === 3 ? 'form' : ''}`}>3</div>
                         </div>
                     </div>
-                    <form className="postarServico1">
-                        <div className="headerVoltar">
-                            <div className="btnVoltar">
-                                <RiArrowLeftLine className="iconeVoltar" />
+                    {form === 1 && (
+                        <form className="postarServico1">
+                            <div className="headerVoltar">
+                                <div className="btnVoltar" onClick={handleBefore}>
+                                    <RiArrowLeftLine className="iconeVoltar" />
+                                </div>
                             </div>
-                        </div>
-                        <div className="left-rightPostar">
-                            <div className="leftPostar">
-                                <h3 className="tituloServico">Titulo do serviço</h3>
-                                <TextInput
-                                    size={{ width: "35vw", height: "3vw" }}
-                                    onChange={handleChange}
-                                    placeholder={"Busque por serviços"}
-                                />
+                            <div className="left-rightPostar">
+                                <div className="leftPostar">
+                                    <h3 className="tituloServico">Titulo do serviço</h3>
+                                    <TextInput
+                                        size={{ width: "35vw", height: "3vw" }}
+                                        onChange={handleChange}
+                                        placeholder={"Busque por serviços"}
+                                    />
 
-                                <div className="emergente-categorias">
-                                    <div className="emergente">
-                                        <input type="checkBox" />
-                                        Serviço emergente
-                                        <RiQuestionLine className="emergenteDuvida" />
+                                    <div className="emergente-categorias">
+                                        <div className="emergente">
+                                            <input type="checkBox" />
+                                            Serviço emergente
+                                            <RiQuestionLine className="emergenteDuvida" />
+                                        </div>
+                                        <div className="categorias">
+                                            Categorias
+                                            <RiListUnordered />
+
+                                        </div>
                                     </div>
-                                    <div className="categorias">
-                                        Categorias
-                                        <RiListUnordered />
+                                    <h3 className="tituloServico">Descreva o serviço detalhadamente</h3>
+                                    <TextInput
+                                        size={{ width: "35vw", height: "10vw" }}
+                                        onChange={handleChange}
+                                        placeholder={"Exemplo: Eu preciso de um pintor para pintar uma parede externa de 4 metros de altura e 6 metros de largura. A parede é feita de tijolos e precisa ser limpa e preparada antes da pintura. Eu gostaria que a parede fosse pintada com tinta acrílica branca. Já comprei toda a tinta necessária, caso precise de mais tinta posso comprar."}
+                                    />
+                                    <div className="anexo">
+                                        Anexo
+                                        <RiAttachment2 className="iconAnexo" />
 
                                     </div>
                                 </div>
-                                <h3 className="tituloServico">Descreva o serviço detalhadamente</h3>
-                                <TextInput
-                                    size={{ width: "35vw", height: "10vw" }}
-                                    onChange={handleChange}
-                                    placeholder={"Exemplo: Eu preciso de um pintor para pintar uma parede externa de 4 metros de altura e 6 metros de largura. A parede é feita de tijolos e precisa ser limpa e preparada antes da pintura. Eu gostaria que a parede fosse pintada com tinta acrílica branca. Já comprei toda a tinta necessária, caso precise de mais tinta posso comprar."}
-                                />
-                                <div className="anexo">
-                                    Anexo
-                                    <RiAttachment2 className="iconAnexo" />
-
+                                <div className="rightPostar">
+                                    <div className="btnProximo" onClick={handleNext}>Próximo</div>
                                 </div>
                             </div>
-                            <div className="rightPostar">
-                                <div className="btnProximo">Próximo</div>
-                            </div>
-                        </div>
-                    </form>
+                        </form>
+                    )}
 
-
-                    <form className="postarServico1">
-                        <div className="headerVoltar">
-                            <div className="btnVoltar">
-                                <RiArrowLeftLine className="iconeVoltar" />
-                            </div>
-                        </div>
-                        <div className="left-rightPostar">
-                            <div className="leftPostar">
-                                <h3 className="tituloServico">Endereço</h3>
-                                <h4 className="postarH4">CEP</h4>
-                                <TextInput
-                                    size={{ width: "8vw", height: "3vw" }}
-                                    onChange={handleChange}
-                                    placeholder={""}
-                                />
-
-                                <h4 className="postarH4">Estado - Cidade</h4>
-                                <TextInput
-                                    size={{ width: "30vw", height: "3vw" }}
-                                    onChange={handleChange}
-                                    placeholder={""}
-                                />
-
-                                <h4 className="postarH4">Bairro</h4>
-                                <TextInput
-                                    size={{ width: "30vw", height: "3vw" }}
-                                    onChange={handleChange}
-                                    placeholder={""}
-                                />
-
-                                <div className="rua-numero">
-                                    <div>
-                                <h4 className="postarH4">Nome da rua</h4>
-                                <TextInput
-                                    size={{ width: "17vw", height: "3vw" }}
-                                    onChange={handleChange}
-                                    placeholder={""}
-                                />
-                                </div>
-                                <div>
-                                 <h4 className="postarH4">Número</h4>
-                                <TextInput
-                                    size={{ width: "8vw", height: "3vw" }}
-                                    onChange={handleChange}
-                                    placeholder={""}
-                                />
-                                </div>
+                    {form === 2 && (
+                        <form className="postarServico1">
+                            <div className="headerVoltar">
+                                <div className="btnVoltar"onClick={handleBefore}>
+                                    <RiArrowLeftLine className="iconeVoltar" />
                                 </div>
                             </div>
-                            <div className="rightPostar" id="rightPostar2">
-                            <h4 className="postarH4">Leve o indicador até sua residência</h4>
-                                <div className="mapa">
-                                </div>
-                                <div className="zoom">
-                                    <div className="mais-menos">+</div>
-                                    <div className="mais-menos">-</div>
+                            <div className="left-rightPostar">
+                                <div className="leftPostar">
+                                    <h3 className="tituloServico">Endereço</h3>
+                                    <h4 className="postarH4">CEP</h4>
+                                    <TextInput
+                                        size={{ width: "8vw", height: "3vw" }}
+                                        onChange={handleChange}
+                                        placeholder={""}
+                                    />
 
-                                </div>
-                                <div className="btnProximo">Próximo</div>
-                            </div>
-                        </div>
-                    </form>
+                                    <h4 className="postarH4">Estado - Cidade</h4>
+                                    <TextInput
+                                        size={{ width: "30vw", height: "3vw" }}
+                                        onChange={handleChange}
+                                        placeholder={""}
+                                    />
 
-                    <form className="postarServico1">
-                        <div className="headerVoltar">
-                            <div className="btnVoltar">
-                                <RiArrowLeftLine className="iconeVoltar" />
-                            </div>
-                        </div>
-                        <div className="left-rightPostar">
-                            <div className="leftPostar">
-                                <h3 className="tituloServico">Previsão de data</h3>
-                                <div className="inicio-fim">
-                                    <div className="inicio">
-                                <h4 className="postarH4">Inicio</h4>
-                                <div className="anexo">
-                                </div>
-                                </div>
-                                <div className="fim">
-                                <h4 className="postarH4">fim</h4>
-                                <div className="anexo">
-                                </div>
-                                </div>
-                                </div>
+                                    <h4 className="postarH4">Bairro</h4>
+                                    <TextInput
+                                        size={{ width: "30vw", height: "3vw" }}
+                                        onChange={handleChange}
+                                        placeholder={""}
+                                    />
 
-                                <h3 className="tituloServico">Pretensão de valores</h3>
-                                <h4 className="pretensaoH4">O quanto você pretende pagar (Esse valor não é definitivo)</h4>
-                                <div className="inicio-fim">
-                                    <div className="inicio">
-                                <h4 className="postarH4">Inicio</h4>
-                                <div className="anexo">
+                                    <div className="rua-numero">
+                                        <div>
+                                            <h4 className="postarH4">Nome da rua</h4>
+                                            <TextInput
+                                                size={{ width: "17vw", height: "3vw" }}
+                                                onChange={handleChange}
+                                                placeholder={""}
+                                            />
+                                        </div>
+                                        <div>
+                                            <h4 className="postarH4">Número</h4>
+                                            <TextInput
+                                                size={{ width: "8vw", height: "3vw" }}
+                                                onChange={handleChange}
+                                                placeholder={""}
+                                            />
+                                        </div>
+                                    </div>
                                 </div>
-                                </div>
-                                <div className="fim">
-                                <h4 className="postarH4">fim</h4>
-                                <div className="anexo">
-                                </div>
-                                </div>
+                                <div className="rightPostar" id="rightPostar2">
+                                    <h4 className="postarH4">Leve o indicador até sua residência</h4>
+                                    <div className="mapa">
+                                    </div>
+                                    <div className="zoom">
+                                        <div className="mais-menos">+</div>
+                                        <div className="mais-menos">-</div>
+
+                                    </div>
+                                    <div className="btnProximo" onClick={handleNext}>Próximo</div>
                                 </div>
                             </div>
-                            <div className="rightPostar">
-                                <div className="btnProximo">Publicar</div>
+                        </form>
+                    )}
+
+                    {form === 3 && (
+                        <form className="postarServico1">
+                            <div className="headerVoltar">
+                                <div className="btnVoltar" onClick={handleBefore}>
+                                    <RiArrowLeftLine className="iconeVoltar" />
+                                </div>
                             </div>
-                        </div>
-                    </form>
+                            <div className="left-rightPostar">
+                                <div className="leftPostar">
+                                    <h3 className="tituloServico">Previsão de data</h3>
+                                    <div className="inicio-fim">
+                                        <div className="inicio">
+                                            <h4 className="postarH4">Inicio</h4>
+                                            <div className="anexo">
+                                            </div>
+                                        </div>
+                                        <div className="fim">
+                                            <h4 className="postarH4">fim</h4>
+                                            <div className="anexo">
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <h3 className="tituloServico">Pretensão de valores</h3>
+                                    <h4 className="pretensaoH4">O quanto você pretende pagar (Esse valor não é definitivo)</h4>
+                                    <div className="inicio-fim">
+                                        <div className="inicio">
+                                            <h4 className="postarH4">Inicio</h4>
+                                            <div className="anexo">
+                                            </div>
+                                        </div>
+                                        <div className="fim">
+                                            <h4 className="postarH4">fim</h4>
+                                            <div className="anexo">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="rightPostar">
+                                    <div className="btnProximo">Publicar</div>
+                                </div>
+                            </div>
+                        </form>
+                    )}
                 </div>
             </div>
         </>
