d036b6b8060f30a9c2ee580dc959b3a936bb9b3b
Leandro Santana <ls1393424@gmail.com>
2024-04-14 14:09:56 -0300

    perfil

--- client/src/components/Infoinc/Infoinc.jsx
+++ client/src/components/Infoinc/Infoinc.jsx
@@ -5,48 +5,9 @@ import { IoMdClose } from "react-icons/io";
 import './infoInc.css';
 export const Infoinc = () => {
   const [infoDados, setinfoDados] = useState();
-
-  const [formData, setFormData] = useState({
-    idCliente: null,
-    email: null,
-    senha: null,
-  });
   const [openModalPerfil, setOpenModalPerfil] = useState(false);
   const [message, setMessage] = useState(null);
-  console.log(formData)
-  const fetchDataFromBackend = async () => {
-    try {
-      const response = await axios.post(
-        "http://localhost:5000/auth/buscarattcls", formData
-      );
-      if (response.status === 202) {
-        setinfoDados(true);
-      }
-    } catch (error) {
-      console.error("Erro ao buscar dados do backend:", error);
-    }
-  };
-
-  useEffect(() => {
-    fetchDataFromBackend();
-    getCookieData();
-  }, []);
-
-
-  const getCookieData = () => {
-    const userDataString = decodeURIComponent(Cookies.get("user"));
-    if (userDataString) {
-      const userData = JSON.parse(userDataString);
-      // Atualize o estado formData com os dados do cookie
-      setFormData((prevFormData) => ({
-        ...prevFormData,
-        idCliente: userData.id_cliente,
-        email: userData.email,
-      }));
-      console.log(userData.id_cliente);
-      console.log(userData.email);
-    }
-  };
+ 
 
   console.log(infoDados)
 
@@ -58,22 +19,6 @@ export const Infoinc = () => {
     }));
   }
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await axios.post('http://localhost:5000/auth/completeLogin', formData);
-      if (response.status === 200) {
-        setMessage(<p className='fail'>{response.data.message}</p>);
-      } else if (response.status === 201) {
-        console.log("Cookie recebido:", response.data); // Verifique o conteúdo do cookie
-        Cookies.set('user', JSON.stringify(response.data));
-        navigate('/homeCliente');
-      }
-    } catch (error) {
-      console.error(error.response.data.message);
-    }
-  };
-
   return (
     <>
       {infoDados && (
@@ -84,7 +29,7 @@ export const Infoinc = () => {
           />  
            <h2>Conclua seu registro.</h2>
             <h5>personalize sua conta para uma melhor experiência</h5>
-          <form onSubmit={handleSubmit}>
+          <form>
             <div className="social-container"></div>
             {message && <div className="container-mensagem-erro">{message}</div>}
             <input placeholder="Digite seu email" type="email" name="email" value={formData.email} onChange={handleChange} />
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Router from './Router'
+import { AuthContext, AuthContextProvider } from "./context/AuthContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <AuthContextProvider>
     <Router/>
+    </AuthContextProvider>
   </React.StrictMode>
 );
