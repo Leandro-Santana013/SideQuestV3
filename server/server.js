a0f10885604a0bd4ab421a589926058642e02729
VitorGerrard <vitorgerrard27@gmail.com>
2024-04-18 21:11:54 -0300

    heheheha
    mongo

--- server/server.js
+++ server/server.js
@@ -1,16 +1,14 @@
-// app.js
 const app = require('./config/express.js');
 const {connectionDataBase} = require("./database/db.js")
-const authController = require ('./routes/auth.js');
-
-
-
+const authController = require('./routes/auth.js');
+const {connectMongo} = require('./database/mongodb.js')
 
 app.use('/auth', authController);  // Em seguida, use o roteador de pages.js para o caminho raiz
 
 
 (async () => {
     await connectionDataBase.sync()
+    connectMongo()
     
     connectionDataBase.authenticate().then(() => {
       console.log("Conexão bem sucedida")
