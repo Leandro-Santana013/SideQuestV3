da2dcd5efcfef574b3f7c345fd80eb4cf2db20b1
Guilherme Saltão <guisaltao@hotmail.com>
2024-04-21 12:03:54 -0300

    responsividade da landing 95% feita, so falta alguns ajustes

--- client/src/components/HeaderLanding/HeaderLanding.jsx
+++ client/src/components/HeaderLanding/HeaderLanding.jsx
@@ -3,7 +3,16 @@ import logo from "../..//assets/logo_SideQuest.png";
 import { Link } from "react-router-dom";
 import "./HeaderLanding.css";
 import { setIsSignUpActive, useIsSignUpActive } from "./singUpState";
+
+
+
 export const HeaderLanding = () => {
+  const [isActive, setIsActive] = useState(false); //isActive começa como falso, setIsActive muda o valor de isactive ao mudar os estado
+
+  const AcaoAoCLicar = () => {
+      setIsActive(!isActive); // Alterna o valor de isActive
+  };
+
   const isSignUpActive = useIsSignUpActive();
 
   const handleLoginClick = (isSignUpActiveValue) => {
@@ -20,6 +29,7 @@ export const HeaderLanding = () => {
 
 
   return (
+    <>
     <header className="containerNavHome">
       <nav className="navHomenav">
         <ul className="navHomeUl">
@@ -49,5 +59,27 @@ export const HeaderLanding = () => {
         </ul>
       </nav>
     </header>
+
+
+
+    <header className="menuResp">
+      <nav className="nav-resp">
+        <ul className={`respNavUl ${isActive ? "active" : ""}`} onClick={AcaoAoCLicar}>
+            <li className="respNavLi">Encontrar serviços</li>
+            <li className="respNavLi"><a href="/landingProfissional"><Link to={"/landingProfissional"}>Trabalhe conosco</Link></a></li>
+            <li className="respNavLi">
+              <a href="/login">
+                <Link to={"/login"} onClick={handleSignInClick}>Login</Link>
+              </a>
+              </li>
+            <li className="respNavLi"> 
+            <a href="/login" class="tab-link link">
+                <Link to={"/login"} onClick={handleSignUpClick}>Inscreva-se</Link>
+              </a></li>
+        </ul>
+      </nav>
+      <button className={`hamburguer-resp ${isActive ? "active" : ""}`} onClick={AcaoAoCLicar}></button>
+    </header>
+    </>
   );
 };
--- server/database/db.js
+++ server/database/db.js
@@ -2,7 +2,7 @@ const { password, name, usu } = require("../config/connectInfo.js")
 const { Sequelize, QueryTypes } = require("sequelize")
 
 const connectionDataBase = new Sequelize(
-    `mysql://${usu}:${password}@localhost:3307/${name}`
+    `mysql://${usu}:${password}@localhost:3306/${name}` //mudar porta
 )
 
 module.exports = { connectionDataBase }
