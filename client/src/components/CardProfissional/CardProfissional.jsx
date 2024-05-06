32bfa0e972a369812d0480438360d4bc751b614b
VitorGerrard <vitorgerrard27@gmail.com>
2024-05-06 00:15:19 -0300

    alterações de css

--- client/src/components/CardProfissional/CardProfissional.jsx
+++ client/src/components/CardProfissional/CardProfissional.jsx
@@ -28,31 +28,31 @@ export const CardProfissional = () => {
     setFiltrosSelecionados([]); // Redefine os filtros ao fechar o modal
     aplicarFiltros(); // Aplica os filtros redefinidos
   };
-  
+
   // Função para aplicar os filtros selecionados
   const aplicarFiltros = () => {
-  // Lógica para aplicar os filtros selecionados
-  const parametrosFiltro = {
-    maisBemAvaliados: filtrosSelecionados.includes('maisBemAvaliados'),
-    maisServicosPrestados: filtrosSelecionados.includes('maisServicosPrestados'),
-    profissionaisFemininas: filtrosSelecionados.includes('profissionaisFemininas'),
-  };
-  setFiltrado(parametrosFiltro);
+    // Lógica para aplicar os filtros selecionados
+    const parametrosFiltro = {
+      maisBemAvaliados: filtrosSelecionados.includes('maisBemAvaliados'),
+      maisServicosPrestados: filtrosSelecionados.includes('maisServicosPrestados'),
+      profissionaisFemininas: filtrosSelecionados.includes('profissionaisFemininas'),
+    };
+    setFiltrado(parametrosFiltro);
   };
 
   // Modifique a função fetchDataFromBackend para passar os filtros como parâmetros
   const fetchDataFromBackend = async () => {
-  try {
-    const response = await axios.get(
-      "http://localhost:5000/user/profissionaisCard", { params: filtrados }
-    );
-    setDadosIniciais(response.data);
+    try {
+      const response = await axios.get(
+        "http://localhost:5000/user/profissionaisCard", { params: filtrados }
+      );
+      setDadosIniciais(response.data);
 
-  } catch (error) {
-    console.error("Erro ao buscar dados do backend:", error);
-    // Tratamento de erro adicional conforme necessário
-  }
-} ;
+    } catch (error) {
+      console.error("Erro ao buscar dados do backend:", error);
+      // Tratamento de erro adicional conforme necessário
+    }
+  };
 
   useEffect(() => {
     fetchDataFromBackend();
@@ -70,14 +70,14 @@ export const CardProfissional = () => {
 
   // Atualize a função filtrarCards para adicionar ou remover filtros selecionados
   const filtrarCards = (filtro) => {
-  let novosFiltros = [...filtrosSelecionados];
-  if (novosFiltros.includes(filtro)) {
-    novosFiltros = novosFiltros.filter(f => f !== filtro);
-  } else {
-    novosFiltros.push(filtro);
-  }
-  setFiltrosSelecionados(novosFiltros);
-};
+    let novosFiltros = [...filtrosSelecionados];
+    if (novosFiltros.includes(filtro)) {
+      novosFiltros = novosFiltros.filter(f => f !== filtro);
+    } else {
+      novosFiltros.push(filtro);
+    }
+    setFiltrosSelecionados(novosFiltros);
+  };
 
   const [coresBotoes, setCoresBotoes] = useState({});
 
@@ -103,17 +103,53 @@ export const CardProfissional = () => {
               <div className="modal-content">
                 <span>Filtre por:</span>
                 <div className="container-card-filtros">
-                  <button className="card-filtro" onClick={() => { filtrarCards('maisBemAvaliados'); alternarCor('botao1') }} style={{ backgroundColor: coresBotoes['botao1'] || 'var(--verde)' }}>
-                    <p><strong>Mais bem avaliados</strong></p>
+                  <button
+                    className="card-filtro"
+                    onClick={() => {
+                      filtrarCards("maisBemAvaliados");
+                    }}
+                    style={{
+                      backgroundColor: filtrosSelecionados.includes("maisBemAvaliados")
+                        ? "var(--azul)"
+                        : "var(--verde)",
+                    }}
+                  >
+                    <p>
+                      <strong>Mais bem avaliados</strong>
+                    </p>
                   </button>
-                  <button className="card-filtro" onClick={() => { filtrarCards('maisServicosPrestados'); alternarCor('botao2') }} style={{ backgroundColor: coresBotoes['botao2'] || 'var(--verde)' }}>
-                    <p><strong>4+ estrelas</strong></p>
+                  <button
+                    className="card-filtro"
+                    onClick={() => {
+                      filtrarCards("maisServicosPrestados")
+                    }}
+                    style={{
+                      backgroundColor: filtrosSelecionados.includes("maisServicosPrestados")
+                        ? "var(--azul)"
+                        : "var(--verde)",
+                    }}
+                  >
+                    <p>
+                      <strong>4+ estrelas</strong>
+                    </p>
                   </button>
-                  <button className="card-filtro" onClick={() => { filtrarCards('profissionaisFemininas'); alternarCor('botao3') }} style={{ backgroundColor: coresBotoes['botao3'] || 'var(--verde)' }}>
-                    <p><strong>Somente profissionais femininas</strong></p>
+                  <button
+                    className="card-filtro"
+                    onClick={() => {
+                      filtrarCards("profissionaisFemininas")
+                    }}
+                    style={{
+                      backgroundColor: filtrosSelecionados.includes("profissionaisFemininas")
+                        ? "var(--azul)"
+                        : "var(--verde)",
+                    }}
+                  >
+                    <p>
+                      <strong>Somente profissionais femininas</strong>
+                    </p>
                   </button>
                 </div>
-                <button className="btn-close-modal" onClick={() => { closeModal(); aplicarFiltros(); }}>Fechar</button>
+                <button className="btn-close-modal" onClick={() => { closeModal(); aplicarFiltros() }}>Fechar</button>
               </div>
             </div>
           )}
@@ -125,14 +161,14 @@ export const CardProfissional = () => {
         </div>
       ) : (
         dadosIniciais.sort((a, b) => b.media_avaliacoes - a.media_avaliacoes).map((profissional) => {
-          if (filtrosSelecionados.includes('profissionaisFemininas') && profissional.sg_sexoProfissional !== 'F') { 
+          if (filtrosSelecionados.includes('profissionaisFemininas') && profissional.sg_sexoProfissional !== 'F') {
             return null;
           }
-          
+
           if (filtrosSelecionados.includes('maisBemAvaliados') && profissional.media_avaliacoes < 4) {
             return null;
           }
-          
+
           // Se o filtro de mais serviços prestados estiver ativado, não exibe os profissionais com menos de 10 serviços realizados -- ALTERAR
           if (filtrosSelecionados.includes('maisServicosPrestados') && profissional.num_servicos_terminados < 10) {
             return null;
@@ -141,44 +177,45 @@ export const CardProfissional = () => {
           // Fechamento da chave final
           return (
             <div className="card-profissional" key={profissional.id_profissional}>
-                          <div className="tamplate-img">
-              <img src={imgPerfil} alt="Imagem de perfil" />
-            </div>
-            <div className="desc-cliente">
-              <div className="perfil-avaliado">
-                <h2>{profissional.nm_profissional}</h2>
-                <div className="stars">
-                  {[...Array(5)].map((_, index) => (
-                    <RiStarFill
-                      key={index}
-                      className={`ri-star-s-fill ${index < profissional.media_avaliacoes ? "ava" : ""
-                        }`}
-                    ></RiStarFill>
-                  ))}
+              <div className="tamplate-img">
+                <img src={imgPerfil} alt="Imagem de perfil" />
+              </div>
+              <div className="desc-cliente">
+                <div className="perfil-avaliado">
+                  <h2>{profissional.nm_profissional}</h2>
+                  <div className="stars">
+                    {[...Array(5)].map((_, index) => (
+                      <RiStarFill
+                        key={index}
+                        className={`ri-star-s-fill ${index < profissional.media_avaliacoes ? "ava" : ""
+                          }`}
+                      ></RiStarFill>
+                    ))}
+                  </div>
+                </div>
+                <div className="emblemas">
+                  <img src={imgMedalha10K} alt="Medalha 10k" />
+                  <img src={imgMedalhaOuro} alt="Medalha de Ouro" />
+                  <img src={imgMedalhaBronze} alt="Medalha de Bronze" />
+                </div>
+                <div className="content-desc">
+                  <p className="desc">{profissional.ds_biografia ? profissional.ds_biografia : "Não possui descrição"}</p>
+                  <span className="vma-vme">ver mais</span>
                 </div>
               </div>
-              <div className="emblemas">
-                <img src={imgMedalha10K} alt="Medalha 10k" />
-                <img src={imgMedalhaOuro} alt="Medalha de Ouro" />
-                <img src={imgMedalhaBronze} alt="Medalha de Bronze" />
-              </div>
-              <div className="content-desc">
-                <p className="desc">{profissional.ds_biografia ? profissional.ds_biografia : "Não possui descrição"}</p>
-                <span className="vma-vme">ver mais</span>
-              </div>
-            </div>
-            <div className="contrate">
-              <button className="btn-contratar">Contratar</button>
-              <div className="serv-realizados">
-                <img src={imgCertificado} alt="certificado" />
-                <p>
-                  Serviços realizados: {profissional.num_servicos_terminados}
-                </p>
+              <div className="contrate">
+                <button className="btn-contratar">Contratar</button>
+                <div className="serv-realizados">
+                  <img src={imgCertificado} alt="certificado" />
+                  <p>
+                    Serviços realizados: {profissional.num_servicos_terminados}
+                  </p>
+                </div>
               </div>
             </div>
-          </div>
           );
         })
       )}
     </section>
-  )}
+  )
+}
