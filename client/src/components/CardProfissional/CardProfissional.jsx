60b268962b901b341d285877c8d94f78cd54df63
VitorGerrard <vitorgerrard27@gmail.com>
2024-05-15 00:05:49 -0300

    correção de card profissional

--- client/src/components/CardProfissional/CardProfissional.jsx
+++ client/src/components/CardProfissional/CardProfissional.jsx
@@ -8,14 +8,9 @@ import imgCertificado from "../../assets/certificado.png";
 import { TextInputBusca } from "../index";
 import axios from 'axios';
 import { Link } from "react-router-dom";
-
-
-
-
 import { RiFilter2Fill, RiStarFill } from "react-icons/ri";
 
 export const CardProfissional = () => {
-
   const [dadosIniciais, setDadosIniciais] = useState([]);
   const [filtrados, setFiltrado] = useState(null);
   const [modal, setModal] = useState(false);
@@ -56,7 +51,6 @@ export const CardProfissional = () => {
         "http://localhost:5000/user/profissionaisCard", { params: filtrados }
       );
       setDadosIniciais(response.data);
-
     } catch (error) {
       console.error("Erro ao buscar dados do backend:", error);
       // Tratamento de erro adicional conforme necessário
@@ -85,12 +79,72 @@ export const CardProfissional = () => {
     });
   };
 
+  const renderizarCards = () => {
+    return dadosIniciais
+      .filter(profissional => {
+        if (filtrosSelecionados.includes('profissionaisFemininas') && profissional.sg_sexoProfissional !== 'F') {
+          return false;
+        }
+
+        if (filtrosSelecionados.includes('maisBemAvaliados') && profissional.media_avaliacoes < 4) {
+          return false;
+        }
+
+        return true;
+      })
+      .sort((a, b) => {
+        if (filtrosSelecionados.includes('maisServicosPrestados')) {
+          return b.num_servicos_terminados - a.num_servicos_terminados;
+        } else {
+          return b.media_avaliacoes - a.media_avaliacoes;
+        }
+      })
+      .map(profissional => (
+        <div className="card-profissional" key={profissional.id_profissional}>
+          <div className="tamplate-img">
+            <img src={imgPerfil} alt="Imagem de perfil" />
+          </div>
+          <div className="desc-cliente">
+            <div className="perfil-avaliado">
+              <h2>{profissional.nm_profissional}</h2>
+              <div className="stars">
+                {[...Array(5)].map((_, index) => (
+                  <RiStarFill
+                    key={index}
+                    className={`ri-star-s-fill ${index < profissional.media_avaliacoes ? "ava" : ""}`}
+                  ></RiStarFill>
+                ))}
+              </div>
+            </div>
+            {/* <div className="emblemas">
+              <img src={imgMedalha10K} alt="Medalha 10k" />
+              <img src={imgMedalhaOuro} alt="Medalha de Ouro" />
+              <img src={imgMedalhaBronze} alt="Medalha de Bronze" />
+            </div> */}
+            <div className="content-desc">
+              <p className="desc">{profissional.ds_biografia ? profissional.ds_biografia : "Não possui descrição"}</p>
+              <span className="vma-vme">ver mais</span>
+            </div>
+          </div>
+          <div className="contrate">
+            <Link to={`/perfilProfissional/${profissional.id_profissional}`}>
+              <button className="btn-contratar">Contratar</button>
+            </Link>
+            <div className="serv-realizados">
+              <img src={imgCertificado} alt="certificado" />
+              <p>Serviços realizados: {profissional.num_servicos_terminados}</p>
+            </div>
+          </div>
+        </div>
+      ));
+  };
+
   return (
     <section className="area-servicos">
       <div className="input-filtros">
         <TextInputBusca placeholder={"Encontre profissionais"} />
         <div className="ifopenf">
-          <div className="filtros" onClick={openModal} style={{display: modal == true ? "none" : 'flex' }}>
+          <div className="filtros" onClick={openModal} style={{ display: modal === true ? "none" : 'flex' }}>
             <p>Filtros</p>
             <RiFilter2Fill className="iconFilter" />
           </div>
@@ -115,69 +169,15 @@ export const CardProfissional = () => {
           )}
         </div>
       </div>
-      {dadosIniciais.length === 0 ? (
-        <div className="sem-profissionais">
-          <p>Nenhum profissional encontrado.</p>
-        </div>
-      ) : (
-        dadosIniciais
-          .filter(profissional => {
-            if (filtrosSelecionados.includes('profissionaisFemininas') && profissional.sg_sexoProfissional !== 'F') {
-              return false;
-            }
-
-            if (filtrosSelecionados.includes('maisBemAvaliados') && profissional.media_avaliacoes < 4) {
-              return false;
-            }
-
-            return true;
-          })
-          .sort((a, b) => {
-            if (filtrosSelecionados.includes('maisServicosPrestados')) {
-              return b.num_servicos_terminados - a.num_servicos_terminados;
-            } else {
-              return b.media_avaliacoes - a.media_avaliacoes;
-            }
-          })
-          .map(profissional => (
-            <div className="card-profissional" key={profissional.id_profissional}>
-              <div className="tamplate-img">
-                <img src={imgPerfil} alt="Imagem de perfil" />
-              </div>
-              <div className="desc-cliente">
-                <div className="perfil-avaliado">
-                  <h2>{profissional.nm_profissional}</h2>
-                  <div className="stars">
-                    {[...Array(5)].map((_, index) => (
-                      <RiStarFill
-                        key={index}
-                        className={`ri-star-s-fill ${index < profissional.media_avaliacoes ? "ava" : ""}`}
-                      ></RiStarFill>
-                    ))}
-                  </div>
-                </div>
-                <div className="emblemas">
-                  <img src={imgMedalha10K} alt="Medalha 10k" />
-                  <img src={imgMedalhaOuro} alt="Medalha de Ouro" />
-                  <img src={imgMedalhaBronze} alt="Medalha de Bronze" />
-                </div>
-                <div className="content-desc">
-                  <p className="desc">{profissional.ds_biografia ? profissional.ds_biografia : "Não possui descrição"}</p>
-                  <span className="vma-vme">ver mais</span>
-                </div>
-              </div>
-              <div className="contrate">
-                <Link to={`/perfilProfissional/${profissional.id_profissional}`}>
-                  <button className="btn-contratar">Contratar</button>
-                </Link>
-                <div className="serv-realizados">
-                  <img src={imgCertificado} alt="certificado" />
-                  <p>Serviços realizados: {profissional.num_servicos_terminados}</p>
-                </div>
-              </div>
-            </div>
-          ))
-      )}
+      <div className="cards-container">
+        {dadosIniciais.length === 0 ? (
+          <div className="sem-profissionais">
+            <p>Nenhum profissional encontrado.</p>
+          </div>
+        ) : (
+          renderizarCards()
+        )}
+      </div>
     </section>
   );
 };
