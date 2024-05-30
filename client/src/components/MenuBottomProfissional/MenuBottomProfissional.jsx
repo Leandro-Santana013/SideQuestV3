c5d4ff239b30d6e9b9b9b08121c8e3cd89e2ac82
VitorGerrard <vitorgerrard27@gmail.com>
2024-05-29 22:46:32 -0300

    correções de css

--- client/src/components/MenuBottomProfissional/MenuBottomProfissional.jsx
+++ client/src/components/MenuBottomProfissional/MenuBottomProfissional.jsx
@@ -1,9 +1,34 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-
+import { IoMenu } from "react-icons/io5";
+import "./MenuBottomProfissional.css";
 
 export const MenuBottomProfissional = () => {
     const [currentPage, setCurrentPage] = useState(window.location.pathname);
+    const [translateMenu, setTranslateMenu] = useState(
+        localStorage.getItem('menuExpanded') === 'true' ? 'translateY(0px)' : 'translateY(220px)'
+    );
+    const [isMobile, setIsMobile] = useState(window.innerWidth <= 815);
+
+    const handleResize = () => {
+        setIsMobile(window.innerWidth <= 815);
+        if (window.innerWidth <= 330) {
+            setTranslateMenu(localStorage.getItem('menuExpanded') === 'true' ? 'translateY(0px)' : 'translateY(290px)');
+        } else {
+            setTranslateMenu(localStorage.getItem('menuExpanded') === 'true' ? 'translateY(0px)' : 'translateY(220px)');
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    const expandirMenu = () => {
+        const newTranslateMenu = translateMenu === 'translateY(0px)' ? (window.innerWidth <= 330 ? 'translateY(290px)' : 'translateY(230px)') : 'translateY(0px)';
+        setTranslateMenu(newTranslateMenu);
+        localStorage.setItem('menuExpanded', newTranslateMenu === 'translateY(0px)');
+    };
 
     const pages = [
         { id: 1, name: "home", href: "homeProfissionais" },
@@ -24,11 +49,12 @@ export const MenuBottomProfissional = () => {
     }, [pages]);
 
     return (
-        <div className="menu-bottom-cliente">
+        <div className={`menu-bottom-profissional ${isMobile ? 'mobile' : ''}`} style={{ transform: isMobile ? translateMenu : 'none', transition: 'all .25s' }}>
             <link
                 rel="stylesheet"
                 href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
             />
+            <button className="btn-expandir-menu-bottom" onClick={() => expandirMenu()}><IoMenu /></button>
             <div className="options-menu-bottom">
                 {pages.map((page) => (
                     <Link
@@ -45,7 +71,7 @@ export const MenuBottomProfissional = () => {
             </div>
         </div>
     );
-};  
+};
 
 const getIcon = (page) => {
     switch (page.href) {
