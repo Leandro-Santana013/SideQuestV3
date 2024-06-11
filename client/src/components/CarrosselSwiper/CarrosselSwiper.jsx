c32a1d69cb61bd1b8b2a07e1c68c8b201810ca72
Arthur Henrique Ribeiro Lacerda <arthur.henrique.r.l.45@gmail.com>
2024-06-11 00:40:22 -0300

    imagens no carrosel de cards da landing page

--- client/src/components/CarrosselSwiper/CarrosselSwiper.jsx
+++ client/src/components/CarrosselSwiper/CarrosselSwiper.jsx
@@ -8,36 +8,51 @@ import "swiper/css/scrollbar";
 import { Navigation } from "swiper/modules";
 import img1 from "../../assets/pintor.png";
 import img2 from "../../assets/substitutir.png";
+import banheiro from "../../assets/banheiro.jpg";
+import calha from "../../assets/calha.jpg";
+import cano from "../../assets/cano.jpg";
+import cano2 from "../../assets/cano2.jpg";
+import deck from "../../assets/deck.jpg";
+import fechadura from "../../assets/fechadura.jpg";
+import fiação from "../../assets/fiação.jpg";
+import grama from "../../assets/grama.jpg";
+import janela from "../../assets/janela.jpg";
+import lampada from "../../assets/lampada.jpg";
+import móveis from "../../assets/móveis.jpg";
+import pintura1 from "../../assets/pintura 1.jpg";
+import prateleira from "../../assets/prateleira.jpg";
+import tomada from "../../assets/tomada.jpg";
+import vazamento from "../../assets/vazamento.jpg";
 import "./CarrosselSwiperStyle.css";
 
 SwiperCore.use([Navigation]);
 
 const allSlidesData = [
   [
-    { id: 1, text: "Instalação de Lâmpadas", image: img1 },
-    { id: 1, text: "Troca de Tomadas", image: img1 },
-    { id: 1, text: "Reparo de Fiação Elétrica", image: img1 },
+    { id: 1, text: "Instalação de Lâmpadas", image: lampada },
+    { id: 1, text: "Troca de Tomadas", image: tomada },
+    { id: 1, text: "Reparo de Fiação Elétrica", image: fiação },
   ],
   [
-    { id: 1, text: "Pintar Parede do Quarto", image: img1 },
-    { id: 2, text: "Pintar Parede da Sala", image: img2 },
+    { id: 1, text: "Pintar Parede do Quarto", image: pintura1 },
+    { id: 2, text: "Pintar Parede da Sala", image: pintura1 },
   ],
   [
-    { id: 1, text: "Conserto de Encanamento", image: img1 },
-    { id: 2, text: "Desentupimento de Canos", image: img2 },
-    { id: 3, text: "Conserto de Vazamento", image: img2 },
+    { id: 1, text: "Conserto de Encanamento", image: cano },
+    { id: 2, text: "Desentupimento de Canos", image: cano2 },
+    { id: 3, text: "Conserto de Vazamento", image: vazamento },
   ],
   [
-    { id: 1, text: "Instalação de Prateleiras", image: img1 },
-    { id: 2, text: "Montagem de Móveis", image: img2 },
-    { id: 3, text: "Reparação Janelas de Madeira", image: img2 },
-    { id: 4, text: "Reparação do Deck", image: img2 },
+    { id: 1, text: "Instalação de Prateleiras", image: prateleira },
+    { id: 2, text: "Montagem de Móveis", image: móveis },
+    { id: 3, text: "Reparação de Janelas", image: janela },
+    { id: 4, text: "Reparação do Deck", image: deck },
   ],
   [
-    { id: 1, text: "Desentupimento de Calhas", image: img1 },
-    { id: 2, text: "Substituição de Fechaduras", image: img2 },
-    { id: 3, text: "Reforma de banheiro", image: img2 },
-    { id: 4, text: "Corte de grama", image: img2 },
+    { id: 1, text: "Desentupimento de Calhas", image: calha },
+    { id: 2, text: "Trocar Fechaduras", image: fechadura },
+    { id: 3, text: "Reforma de banheiro", image: banheiro },
+    { id: 4, text: "Corte de grama", image: grama },
   ],
   [
     { id: 1, text: "Texto do slide 1", image: img1 },
