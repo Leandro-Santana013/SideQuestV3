f9f57a545e05365db7f31a95c1ad90eb0118645e
Leandro Santana <ls1393424@gmail.com>
2024-06-11 03:34:29 -0300

    final

--- client/src/pages/LandingProfissional/LandingProfissional.jsx
+++ client/src/pages/LandingProfissional/LandingProfissional.jsx
@@ -1,3 +1,4 @@
+
 import "./landingProfissional.css";
 import React from "react";
 import { useState } from "react";
@@ -87,14 +88,6 @@ const LandingProfissional = () => {
                             <p className="pe">Converse livremente com seus clientes</p>
                         </div>
                         <div className="beneficios-plano">
-                        <img src={verificado} alt="verificado" />
-                            <p>Acesso à nossa rede de clientes</p>
-                        </div>
-                        <div className="beneficios-plano">
-                        <img src={verificado} alt="verificado" />
-                            <p>Converse livremente com seus clientes</p>
-                        </div>
-                        <div className="beneficios-plano">
                             <img src={verificado} alt="verificado" />
                             <p className="pe">1 categoria para serviços</p>
                         </div>
@@ -121,14 +114,6 @@ const LandingProfissional = () => {
                             <p className="pe">Converse livremente com seus clientes</p>
                         </div>
                         <div className="beneficios-plano">
-                        <img src={verificado} alt="verificado" />
-                            <p>Acesso à nossa rede de clientes</p>
-                        </div>
-                        <div className="beneficios-plano">
-                        <img src={verificado} alt="verificado" />
-                            <p>Converse livremente com seus clientes</p>
-                        </div>
-                        <div className="beneficios-plano">
                             <img src={verificado} alt="verificado" />
                             <p className="pe">4 categorias para serviços</p>
                         </div>
@@ -153,14 +138,6 @@ const LandingProfissional = () => {
                         <div className="beneficios-plano">
                         <img src={verificado} alt="verificado" />
                             <p className="pe">Converse livremente com seus clientes</p>
-                        </div>
-                        <div className="beneficios-plano">
-                        <img src={verificado} alt="verificado" />
-                            <p>Acesso à nossa rede de clientes</p>
-                        </div>
-                        <div className="beneficios-plano">
-                        <img src={verificado} alt="verificado" />
-                            <p>Converse livremente com seus clientes</p>
                         </div>
                         <div className="beneficios-plano">
                             <img src={verificado} alt="verificado" />
